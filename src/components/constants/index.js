7322e17ff36b7b0875ec5bd8ab98c7a478b9e521
shawnho1018 <hos@vmware.com>
2019-04-23 23:56:46 +0800

    change constants

---
+++ src/components/constants/index.js
@@ -0,0 +1,7 @@
+export const PATH_BASE = 'http://api.openweathermap.org/data/2.5';
+export const PATH_SEARCH = '/weather?q=';
+export const PATH_FORECAST = '/forecast/daily?q=';
+export const PARAM_TYPE = 'type=accurate';
+export const PARAM_UNIT = 'units=metric';
+export const PARAM_APPID = 'fab323c742ae2d29019ed9868a2bd5f0';
+export const PARAM_DAYS = '&cnt=5';
